dbf19a1b57dfe494e60d0f52b032ecb4e0402b37
Ryan Blake <xbungalowx@gmail.com>
2022-02-28 19:02:57 -0600

    delete route complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const req = require('express/lib/request')
 const breads = express.Router()
 const Bread = require('../models/bread')
 
@@ -21,7 +22,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex
     })
   } else {
     res.render('notFound')
@@ -38,6 +40,12 @@ breads.post('/', (req, res) => {
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 module.exports = breads
