667a6f88ff1fd12aed016f396c52b67914e6419f
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 09:28:52 -0500

    fixing catch error syntax

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 // DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-  (err) => {if(err) throw err})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}
+  .catch(error => console.log(error)))
 // CREATING EXPRESS APP
 const app = express()
 
