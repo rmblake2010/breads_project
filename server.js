0fb03fb83dcb61e45d146f81786c0427246d43ce
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 08:13:36 -0500

    debugging timeout

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express()
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
-app.use(timeout('30s'))
+app.use(timeout('90s'))
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
