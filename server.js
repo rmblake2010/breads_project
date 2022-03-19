6fcbd70c08e94415b32a5802516f6440a6489166
Ryan Blake <xbungalowx@gmail.com>
2022-03-19 09:32:47 -0500

    debugging mongoose connection check

--- server.js
+++ server.js
@@ -8,8 +8,12 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 // DATABASE CONNECTION
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}
-  .catch(error => console.log(error)))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true},
+  () => console.log('connected to mongo: ', process.env.MONGO_URI))
+mongoose.connection.on('error', err => {
+  logError(err)
+})
+
 // CREATING EXPRESS APP
 const app = express()
 
